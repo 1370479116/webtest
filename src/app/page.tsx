34ba444d1c25e9e3d2fa534c18e245e8591c3e18
1370479116 <130902221+1370479116@users.noreply.github.com>
2026-02-09 12:58:46 +0900

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="h-dvh w-dvw">
+      <iframe
+        src="/index.html"
+        title="WebTest"
+        className="h-full w-full border-0"
+        referrerPolicy="no-referrer"
+      />
+    </main>
+  );
+}
